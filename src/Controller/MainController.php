c882f02e0183a53593fab1e5c678e70893de4c00
dima <yashchuk@rozetka.com.ua>
2022-03-16 08:34:33 +0200

    step 2

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,13 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="app_main")
+     * @Route("/main", name="app_main")
      */
     public function index(): Response
     {
-    echo 'index';
-//         return $this->render('main/index.html.twig', [
-//             'controller_name' => 'MainController',
-//         ]);
+         return $this->render('main/index.html.twig', [
+             'controller_name' => 'MainController',
+         ]);
     }
 }
